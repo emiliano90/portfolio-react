c4668512b0aeb3b8f7ff3e46ef353edaa8b73442
emiliano90 <emiliano_lorusso@hotmail.com>
2024-03-05 17:31:06 -0300

    arreglos

--- src/data/constants.js
+++ src/data/constants.js
@@ -254,32 +254,27 @@ export const experiences = [
 ];
 
 export const education = [
-  {
+	{
     id: 0,
-    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Kiit.jpeg?alt=media&token=3839d520-c59d-4341-ad8f-0980c98de4dd",
-    school: "Kalinga Institute of Industrial Technology, Bhubaneswar",
-    date: "Oct 2021 - Sep 2025",
-    grade: "8.71 CGPA",
-    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Kalinga Institute of Industrial Technology, Bhubaneswar. I have completed 4 semesters and have a CGPA of 8.71. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Google Developers Student Club (GDSC) at KIIT, where I am learning and working on exciting projects with a team of talented developers.",
-    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+    img: "/coderhouse.png",
+    school: "Coderhouse",
+    date: "Janueary 2023 - March 2023",
+    desc: "Component programming, using Javascript, JS, ES6, and advantages of using data streams. Handling routes using Firebase, and understanding the use of the virtual DOM through React JS developments.",
+    degree: "React.js",
   },
   {
     id: 1,
-    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
-    school: "Methodist School, Dankuni",
-    date: "Apr 2019 - Apr 2025",
-    grade: "88.2%",
-    desc: "I completed my class 12 high school education at Methodist School, Dankuni, where I studied Science with Computer Science.",
-    degree: "ISC(XII), Science with Computer",
+    img: "/um.png",
+    school: "Universidad de Moron",
+    date: "2008 - 2016",
+    degree: "Systems Engineer",
   },
+  
   {
     id: 2,
-    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/methodist.png?alt=media&token=018a1b18-e2dd-4f34-8855-cff1b6b69fd3",
-    school: "Methodist School, Dankuni",
-    date: "Apr 2017 - Apr 2019",
-    grade: "92.3%",
-    desc: "I completed my class 10 education at Methodist School, Dankuni, where I studied Science with Computer Application.",
-    degree: "ICSC(X), Science with Computer",
+    img: "estrada.webp",
+    school: "Instituto Jose Manuel Estrada",
+    date: "2007"
   },
 ];
 
